f5b9e90e0eeb808f39c88bcbd22cd7f539111cf5
lenryk <lenry101@gmail.com>
2022-09-20 23:58:22 +0100

    Exercise 1.1 (fix)

---
+++ part1/src/App.js
@@ -0,0 +1,44 @@
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+
+    function Header({course}) {
+        return <h1>{course}</h1>
+    }
+
+    function Content({part1, exercises1, part2, exercises2, part3, exercises3}) {
+        return (
+            <>
+                <p>
+                    {part1} {exercises1}
+                </p>
+                <p>
+                    {part2} {exercises2}
+                </p>
+                <p>
+                    {part3} {exercises3}
+                </p>
+            </>
+            )
+    }
+
+    function Total({exercises1, exercises2, exercises3}) {
+        return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    }
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+        </div>
+    )
+}
+
+export default App
