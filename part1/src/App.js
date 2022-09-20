c87342d597a7fa4abd9efadf655c3a1b6e8d64d1
lenryk <lenry101@gmail.com>
2022-09-21 00:00:54 +0100

    Exercise 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,18 +12,16 @@ const App = () => {
         return <h1>{course}</h1>
     }
 
+    function Part({part, exercise}) {
+        return <p>{part} {exercise}</p>
+    }
+
     function Content({part1, exercises1, part2, exercises2, part3, exercises3}) {
         return (
             <>
-                <p>
-                    {part1} {exercises1}
-                </p>
-                <p>
-                    {part2} {exercises2}
-                </p>
-                <p>
-                    {part3} {exercises3}
-                </p>
+                <Part part={part1} exercise={exercises1} />
+                <Part part={part2} exercise={exercises2} />
+                <Part part={part3} exercise={exercises3} />
             </>
             )
     }
