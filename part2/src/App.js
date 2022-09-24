0615dcc2411aa59bed9b0f6a67dc9e7a3d6ed65c
lenryk <lenry101@gmail.com>
2022-09-24 17:01:32 +0100

    Exercise 2.8

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1234567' }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumbers] = useState('')
 
     function handleAdd(event) {
         event.preventDefault()
@@ -12,7 +13,7 @@ const App = () => {
         if (persons.find(obj => obj.name === newName)) {
             alert(`${newName} is already added to the phonebook`)
         } else {
-            setPersons([...persons, {name:newName}])
+            setPersons([...persons, {name:newName, number: newNumber}])
         }
     }
 
@@ -24,11 +25,14 @@ const App = () => {
                     name: <input onChange={(event) => setNewName(() => event.target.value)} value={newName}/>
                 </div>
                 <div>
+                    number: <input onChange={(event) => setNewNumbers(() => event.target.value)} value={newNumber}/>
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(((person, id) => <p key={id}>{person.name}</p>))}
+            {persons.map((({name, number}, id) => <p key={id}>{name} {number}</p>))}
         </div>
     )
 }
