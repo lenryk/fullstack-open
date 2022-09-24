c7aa51a7094e317f7d161bc6ee974e8505bd64dc
lenryk <lenry101@gmail.com>
2022-09-24 15:03:25 +0100

    Exercise 2.3

--- part2/src/components/Sum/index.jsx
+++ part2/src/components/Sum/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Sum({course}) {
-    return <p>total of {course.parts.reduce((prev, current) => prev+current.exercises, 0)} exercises</p>
+    return <p>total of {course.parts.reduce((prev, current) => prev+current.exercises, 0)} exercises </p>
 }
 
 export default Sum
