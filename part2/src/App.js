1c148924bf402082bed6438fd7e7e3a431e2fe28
lenryk <lenry101@gmail.com>
2022-09-24 16:06:51 +0100

    Exercise 2.6

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,55 +1,30 @@
-import Course from "./components/Course"
+import { useState } from 'react'
 
 const App = () => {
-    const courses = [
-        {
-            name: 'Half Stack application development',
-            id: 1,
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1
-                },
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2
-                },
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3
-                },
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4
-                }
-            ]
-        },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                    name: 'Routing',
-                    exercises: 3,
-                    id: 1
-                },
-                {
-                    name: 'Middlewares',
-                    exercises: 7,
-                    id: 2
-                }
-            ]
-        }
-    ]
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    function handleAdd(event) {
+        event.preventDefault()
+        setPersons([...persons, {name:newName}])
+    }
 
     return (
-        <>
-            {courses.map(course => <Course course={course} />)}
-        </>
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleAdd}>
+                <div>
+                    name: <input onChange={(event) => setNewName(() => event.target.value)} value={newName}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(((person, id) => <p key={id}>{person.name}</p>))}
+        </div>
     )
 }
 
