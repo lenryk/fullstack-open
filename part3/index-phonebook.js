2689e33abbef6016b74a752e175c165658a05956
lenryk <lenry101@gmail.com>
2022-09-28 22:56:37 +0100

    Exercise 3.3

--- part3/index-phonebook.js
+++ part3/index-phonebook.js
@@ -34,6 +34,13 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br/> ${new Date()}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    person ? response.json(person) : response.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
