55b2f82213abc124684cab62f35427a1963a256f
lenryk <lenry101@gmail.com>
2022-09-28 23:02:13 +0100

    Exercise 3.5

--- part3/index-phonebook.js
+++ part3/index-phonebook.js
@@ -48,6 +48,28 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'no content'
+        })
+    }
+
+    const newPerson = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: Math.floor(Math.random() * 1000),
+    }
+
+    persons = persons.concat(newPerson)
+
+    response.json(newPerson)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
