0d0e1f3b441cc31b44e635a577f140fb537bf075
lenryk <lenry101@gmail.com>
2022-09-30 23:20:33 +0100

    Exercise 3.15 + 3.16

--- part3/index-phonebook.js
+++ part3/index-phonebook.js
@@ -10,6 +10,17 @@ app.use(express.json())
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -21,18 +32,20 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br/> ${new Date()}`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 
     Person.findById(request.params.id).then((person) => {
-        response.json(person)
+        person ? response.json(person) : response.status(404).end()
     })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
