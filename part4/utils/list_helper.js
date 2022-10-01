858d22f6afb9863f29eaa4a559b1d76f9b4b9d58
lenryk <lenry101@gmail.com>
2022-10-01 23:50:23 +0100

    Exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,6 +6,10 @@ function totalLikes(blogs) {
     return blogs.reduce((prev, accum) => prev + accum.likes, 0)
 }
 
+function favoriteBlog(blogs) {
+    return result = blogs.sort((a, b) => a.likes - b.likes).at(-1)
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
