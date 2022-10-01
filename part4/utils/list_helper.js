270dff657341642200abd7742a220efe984d9cd1
lenryk <lenry101@gmail.com>
2022-10-01 23:34:43 +0100

    Exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
     return 1
 }
 
+function totalLikes(blogs) {
+    return blogs.reduce((prev, accum) => prev + accum.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
