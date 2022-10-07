60af2827efafa97bc1e1d17f1d577f006e5785c2
lenryk <lenry101@gmail.com>
2022-10-08 00:21:51 +0100

    Exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 require('express-async-errors')
 
@@ -22,6 +23,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 module.exports = app
