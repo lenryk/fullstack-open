dbedc39a84d16c89215e1b57e1b64db7242e2d66
lenryk <lenry101@gmail.com>
2022-10-10 00:09:24 +0100

    Exercise 5.14

--- part5/src/components/Blog/index.test.js
+++ part5/src/components/Blog/index.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './index'
+import userEvent from '@testing-library/user-event/'
 
 test('renders blog title and author', () => {
   const blogData = {
@@ -20,3 +21,24 @@ test('renders blog title and author', () => {
   expect(screen.getByText(/my testing blog/)).toBeVisible()
 })
 
+test('view more button shows likes and url', async () => {
+  const blogData = {
+    title: 'my testing blog',
+    author: 'michael',
+    url: 'google.com',
+    likes: 50,
+    user: {
+      username: 'mike'
+    }
+  }
+
+  localStorage.setItem('user', '{"user": "mike"}')
+
+  render(<Blog blog={blogData} handleLike={() => {}} handleDelete={() => {}} />)
+
+  const user = userEvent.setup()
+  await user.click(screen.getByTestId('viewToggle'))
+
+  expect(screen.getByText(/google.com/)).toBeVisible()
+  expect(screen.getByText(/50/)).toBeVisible()
+})
