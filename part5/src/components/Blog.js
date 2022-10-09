5a2c9c828b9082246ebdd5a660c4f5e90042af39
lenryk <lenry101@gmail.com>
2022-10-09 21:29:49 +0100

    Exercise 5.7 (styles)

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -3,8 +3,16 @@ import {useState} from 'react'
 function Blog({blog}) {
   const [visibility, setVisibility] = useState(false)
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   return (
-      <div>
+      <div style={blogStyle}>
         {blog.title} <button onClick={() => setVisibility(!visibility)}>{visibility ? 'close' : 'view'}</button>
           {visibility && ( <ul>
                 <li>{blog.author}</li>
