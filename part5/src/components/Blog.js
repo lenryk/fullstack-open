88d83d013a6370fb430d39cdcd6ffd170d6ab715
lenryk <lenry101@gmail.com>
2022-10-09 21:27:19 +0100

    Exercise 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,17 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>
-)
+import {useState} from 'react'
+
+function Blog({blog}) {
+  const [visibility, setVisibility] = useState(false)
+
+  return (
+      <div>
+        {blog.title} <button onClick={() => setVisibility(!visibility)}>{visibility ? 'close' : 'view'}</button>
+        <div>
+          {visibility && <span>{blog.author} {blog.url}</span>}
+        </div>
+      </div>
+      )
+
+}
 
 export default Blog
