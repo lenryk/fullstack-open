565c871021c33fc64b920636aea5eec1b97dd5e2
lenryk <lenry101@gmail.com>
2022-10-16 21:27:53 +0100

    Exercise 6.13

--- part6-2/src/App.js
+++ part6-2/src/App.js
@@ -2,8 +2,19 @@ import AnecdoteForm from '../src/components/AnecdoteForm'
 import AnecdoteList from '../src/components/AnecdoteList';
 import Notifications from '../src/components/Notification'
 import Filter from '../src/components/Filter'
+import {useEffect} from 'react'
+import anecdoteService from './services/anecdotes'
+import {useDispatch} from 'react-redux'
+import { saveAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(saveAnecdotes(anecdotes)))
+  }, [dispatch])
+
+
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6-2/src/reducers/anecdoteReducer.js
+++ part6-2/src/reducers/anecdoteReducer.js
@@ -1,29 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 const anecdotesSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     incrementVote(state, action) {
       // search array of objects to find object that matches passed in id param from function below
@@ -43,9 +24,12 @@ const anecdotesSlice = createSlice({
         id: getId(),
         votes: 0
       })
+    },
+    saveAnecdotes(state, action) {
+      return action.payload
     }
   },
 })
 
-export const {incrementVote, addNewAnecdote } = anecdotesSlice.actions
+export const {incrementVote, addNewAnecdote, saveAnecdotes } = anecdotesSlice.actions
 export default anecdotesSlice.reducer
