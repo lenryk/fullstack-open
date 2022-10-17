52880578cc00098b354ae45e226a6bce8747e012
lenryk <lenry101@gmail.com>
2022-10-17 23:14:39 +0100

    Exercise 6.19

--- part6-2/src/components/Notification.js
+++ part6-2/src/components/Notification.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
 const Notification = () => {
   const notification = useSelector(state => state.notifications)
@@ -14,4 +15,5 @@ const Notification = () => {
 
 }
 
-export default Notification
+const ConnectedNotification = connect()(Notification)
+export default ConnectedNotification
