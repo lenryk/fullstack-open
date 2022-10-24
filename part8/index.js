85effee9042525c6f84962fc0930944e54f061bd
lenryk <lenry101@gmail.com>
2022-10-24 23:38:17 +0100

    Exercise 8.2

--- part8/index.js
+++ part8/index.js
@@ -93,9 +93,18 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Books {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `;
 
@@ -103,6 +112,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
