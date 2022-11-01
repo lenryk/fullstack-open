1eb1b5163399559c3d4b3201f0f7ce8fbabcd308
lenryk <lenry101@gmail.com>
2022-11-01 23:59:24 +0000

    Exercise 9.5

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,10 +1,11 @@
 const calculateBmi = (height: number, weight: number): string => {
   const result = weight / ((height / 100) ^ 2);
-  console.log(result, height, weight);
   if (result <= 18.5) return "Underweight";
   if (result > 18.5 && result <= 24.9) return "Normal weight";
   if (result >= 25 && result <= 29.9) return "Overweight";
-  if (result >= 30) return "Obese";
+  else return "Obese";
 };
 
-console.log(calculateBmi(Number(process.argv[2]), Number(process.argv[3])));
+// console.log(calculateBmi(Number(process.argv[2]), Number(process.argv[3])));
+
+export { calculateBmi };
--- part9/index.ts
+++ part9/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.get("/hello", (_, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+  if (Number.isNaN(Number(weight)) || Number.isNaN(Number(height)))
+    return res.status(400).json({ error: "Malformatted parameters" });
+
+  const bmi = calculateBmi(Number(height), Number(weight));
+  return res.json({ height: Number(height), weight: Number(weight), bmi });
 });
 
 const PORT = 3003;
