2c1c305db1da33f252fe67b61df41df4310e3978
lenryk <lenry101@gmail.com>
2022-11-03 22:27:10 +0000

    Exercise 9.15

--- part9-4/src/App.tsx
+++ part9-4/src/App.tsx
@@ -1,17 +1,58 @@
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CoursePartBaseDesc extends CoursePartBase {
+    description: string;
+  }
+
+  interface CourseNormalPart extends CoursePartBaseDesc {
+    type: "normal";
+  }
+
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartBaseDesc {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
     },
   ];
 
@@ -19,30 +60,54 @@ const App = () => {
     return <h1>{courseName}</h1>;
   }
 
-  type courseParts = {
-    name: string;
-    exerciseCount: number;
-  };
+  function Part({ part }: { part: CoursePart }) {
+    switch (part.type) {
+      case "normal":
+        return (
+          <>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>{part.description}</p>
+          </>
+        );
+      case "groupProject":
+        return (
+          <>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>project exercises {part.groupProjectCount}</p>
+          </>
+        );
+      case "submission":
+        return (
+          <>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>{part.description}</p>
+            <p>{part.exerciseSubmissionLink}</p>
+          </>
+        );
+    }
+  }
 
-  function Content({ courseParts }: { courseParts: Array<courseParts> }) {
+  function Content({ courseParts }: { courseParts: Array<CoursePart> }) {
     return (
       <>
         {" "}
         {courseParts.map((part) => {
-          return (
-            <p key={part.exerciseCount}>
-              {part.name} {part.exerciseCount}
-            </p>
-          );
+          return <Part part={part} key={part.name} />;
         })}
       </>
     );
   }
 
-  function Total({ courseParts }: { courseParts: Array<courseParts> }) {
+  function Total({ courseParts }: { courseParts: Array<CoursePart> }) {
     return (
       <p>
-        Number of exercises{" "}
+        Number of exercises:{" "}
         {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
       </p>
     );
