7a695ec38a5d497282e4dc43d5b716856e2621e2
lenryk <lenry101@gmail.com>
2022-11-04 22:28:53 +0000

    Exercise 10.5

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,9 +1,20 @@
-import { Text } from "react-native";
+import { Text, Image, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+});
 
 const RepositoryItem = ({ item }) => {
   console.log(item);
   return (
     <>
+      <Image style={styles.logo} source={item.ownerAvatarUrl} />
       <Text>Full name: {item.fullName}</Text>
       <Text>Description: {item.description}</Text>
       <Text>Language: {item.language}</Text>
